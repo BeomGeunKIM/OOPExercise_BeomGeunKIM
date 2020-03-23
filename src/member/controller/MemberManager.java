68c3839e09179859c0af3f9128a8b5594692b620
beom <beom@DESKTOP-4P8F1ND>
2020-03-23 21:35:57 +0900

    MemberManager insertMember 메소드 추가

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -13,7 +13,24 @@ public class MemberManager {
 	public MemberManager() {}
 	
 	public void insertMember() {
+		System.out.print("아이디를 입력하세요 : ");
+		String userId = sc.next();
+		System.out.print("패스워드를 입력하세요 : ");
+		String userPwd = sc.next();
+		System.out.print("이름을 입력하세요 : ");
+		String userName = sc.next();
+		System.out.print("나이를 입력하세요 : ");
+		int age = sc.nextInt();
+		System.out.print("성별을 입력하세요 : ");
+		char gender = sc.next().charAt(0);
+		sc.nextLine();
+		System.out.print("이메일을 입력하세요 : ");
+		String email = sc.nextLine();
 		
+		m[cnt] = new Member(userId, userPwd, userName, age, gender, email);
+		cnt++;
+		
+		System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다."); return;
 	}
 	
 	public void searchId() {
